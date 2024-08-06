72a1e19a6b06505211f0152dd0f43418ecbf0489
Ezequiel Grandia Plastimold <ezequielgrandia@gmail.com>
2024-08-06 09:25:12 -0300

    Renders and JS fixed unified

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("./json/90.json")
+    const jsonPath = window.location.pathname.includes("215.html") ? "./json/215.json" : "./json/90.json";
+    const barClass = window.location.pathname.includes("215.html") ? "diagonal-bar-215" : "diagonal-bar-90";
+
+    fetch(jsonPath)
         .then((response) => response.json())
-        .then((data) => renderPuzzles(data))
+        .then((data) => renderPuzzles(data, barClass))
         .catch((error) => console.error("Error al cargar el archivo JSON:", error));
 });
 
-function renderPuzzles(puzzles) {
+function renderPuzzles(puzzles, barClass) {
     const container = document.querySelector(".imgs-container");
     puzzles.forEach((puzzle) => {
         const card = document.createElement("div");
         card.classList.add("card");
         card.setAttribute("data-stock", puzzle.stock);
+
+        const imgWrapper = document.createElement("div");
+        imgWrapper.classList.add("puzzle-img-wrapper");
 
         const img = document.createElement("img");
         img.src = puzzle.img;
@@ -24,6 +30,13 @@ function renderPuzzles(puzzles) {
             img.addEventListener("click", openFullscreen);
         }
 
+        const diagonalBar = document.createElement("div");
+        diagonalBar.classList.add(barClass);
+
+        imgWrapper.appendChild(img);
+        imgWrapper.appendChild(diagonalBar);
+        card.appendChild(imgWrapper);
+
         const h3 = document.createElement("h3");
         h3.classList.add("card__h3");
         h3.textContent = puzzle.nombre;
@@ -36,7 +49,6 @@ function renderPuzzles(puzzles) {
         pStock.classList.add("card__p");
         pStock.textContent = `Stock: ${puzzle.stock}`;
 
-        card.appendChild(img);
         card.appendChild(h3);
         card.appendChild(pCode);
         card.appendChild(pStock);
